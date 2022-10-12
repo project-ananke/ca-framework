56d9b9b6c8bd9f1b4f121dabe811cfcc9dfaa789
sir-w7 <vohwinston@gmail.com>
2022-10-11 21:50:07 -0700

    Switched to static linking for SDL2.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pushd source/engine/stygatore > /dev/null
-go build 
+go build
 popd > /dev/null
 
 mkdir -p build
@@ -10,4 +10,4 @@ mv source/engine/stygatore/stygatore build/
 ./build/stygatore source/engine/generated
 
 echo "build/styx_platform <- source/platform.c"
-clang -Isource/engine -Isource/engine/ext `sdl2-config --cflags --libs` -Wno-incompatible-pointer-types-discards-qualifiers ./source/platform.c -o build/styx_platform
+clang -Isource/engine -Isource/engine/ext `sdl2-config --cflags --static-libs` -Wno-incompatible-pointer-types-discards-qualifiers ./source/platform.c -o build/styx_platform
