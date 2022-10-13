4f130366b829eed19e44a632559c442996844634
sir-w7 <vohwinston@gmail.com>
2022-10-12 21:45:21 -0700

    Fixed build.bat.

--- build.bat
+++ build.bat
@@ -1,7 +1,6 @@
 @echo off
 
+if not exist build mkdir build
 pushd build
-
 cl /nologo /I..\ext\SDL2\include ..\source\main.c /link /LIBPATH:..\ext\lib\x64 SDL2.lib SDL2main.lib /out:ananke.exe
-
 popd
