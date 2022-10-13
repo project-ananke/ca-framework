f7bb63984b5d19bd966332ad87f1a5e4eef5c9a0
sir-w7 <vohwinston@gmail.com>
2022-10-12 22:02:28 -0700

    Copy the SDL2.dll over into the shipping directory.

--- build.bat
+++ build.bat
@@ -2,5 +2,8 @@
 
 if not exist build mkdir build
 pushd build
+
 cl /nologo /I..\ext\SDL2\include ..\source\main.c /link /LIBPATH:..\ext\SDL2\lib\x64 SDL2.lib SDL2main.lib Shell32.lib /SUBSYSTEM:CONSOLE /out:ananke.exe
+copy ..\ext\SDL2\lib\SDL2.dll .
+
 popd
