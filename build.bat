6ab35512ddba53131b8ef60fe77638e31d98be3e
sir-w7 <vohwinston@gmail.com>
2022-10-12 21:41:52 -0700

    Rule 30 working??? Though buggy.

--- build.bat
+++ build.bat
@@ -1,25 +1,7 @@
 @echo off
 
-set compile_flags= /nologo /Zi /FC /W4 /WX /wd4100 /wd4189 /wd4201 /wd4115 /wd4211 /wd4311 /I ..\source\engine
-set link_flags= /opt:ref /incremental:no
-
-set platform_link_flags= /subsystem:windows shlwapi.lib user32.lib gdi32.lib dxguid.lib d3d11.lib dxgi.lib d3dcompiler.lib %link_flags% 
-
-REM What if we only use DLLs to minimize the exe size?
-if not exist build mkdir build
-
-pushd source\engine\stygatore
-go build
-popd
-
-move source\engine\stygatore\stygatore.exe build\
-
 pushd build
 
-echo -- Compiling Styx platform layer. 
-cl %compile_flags% ..\source\platform.c /link %platform_link_flags% /out:styx_platform.exe
-
-echo -- Cleaning up miscellaneous files.
-del /s /q *.obj *.lib *.exp vc140.pdb
+cl /nologo /I..\ext\SDL2\include ..\source\main.c /link /LIBPATH:..\ext\lib\x64 SDL2.lib SDL2main.lib /out:ananke.exe
 
 popd
--- source/main.c
+++ source/main.c
@@ -3,7 +3,73 @@
 #include <SDL.h>
 
 SDL_Window *window;
+SDL_Renderer *renderer;
+
 bool running = true;
+
+#define GRID_WIDTH 64
+#define GRID_HEIGHT 36
+int grid[GRID_HEIGHT][GRID_WIDTH + 2];
+
+void render_grid()
+{
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+	for (int y = 0; y < GRID_HEIGHT; ++y) {
+		for (int x = 0; x < GRID_WIDTH; ++x) {
+			bool fill = (grid[y][x + 1] == 1);
+			
+			SDL_Rect fill_rect = { x * 20, y * 20, 20, 20 };
+			if (fill) {
+				SDL_RenderFillRect(renderer, &fill_rect);
+			} else {
+				SDL_RenderDrawRect(renderer, &fill_rect);
+			}
+		}
+	}
+}
+
+void rule_30() 
+{
+	grid[0][GRID_WIDTH / 2] = 1;
+
+	for (int y = 1; y < GRID_HEIGHT; ++y) {
+		for (int x = 1; x < GRID_WIDTH - 1; ++x) {
+			if (grid[y - 1][x - 1] == 1 &&
+				grid[y - 1][x]     == 1 &&
+				grid[y - 1][x + 1] == 1) {
+				grid[y][x] = 0;
+			} else if (grid[y - 1][x - 1] == 1 &&
+				       grid[y - 1][x]     == 1 &&
+				       grid[y - 1][x + 1] == 0) {
+				grid[y][x] = 0;
+			} else if (grid[y - 1][x - 1] == 1 &&
+				       grid[y - 1][x]     == 0 &&
+				       grid[y - 1][x + 1] == 1) {
+				grid[y][x] = 0;
+			} else if (grid[y - 1][x - 1] == 1 &&
+				       grid[y - 1][x]     == 0 &&
+				       grid[y - 1][x + 1] == 0) {
+				grid[y][x] = 1;
+			} else if (grid[y - 1][x - 1] == 0 &&
+				       grid[y - 1][x]     == 1 &&
+				       grid[y - 1][x + 1] == 1) {
+				grid[y][x] = 1;
+			} else if (grid[y - 1][x - 1] == 0 &&
+				       grid[y - 1][x]     == 1 &&
+				       grid[y - 1][x + 1] == 0) {
+				grid[y][x] = 1;
+			} else if (grid[y - 1][x - 1] == 0 &&
+				       grid[y - 1][x]     == 0 &&
+				       grid[y - 1][x + 1] == 1) {
+				grid[y][x] = 1;
+			} else if (grid[y - 1][x - 1] == 0 &&
+				       grid[y - 1][x]     == 0 &&
+				       grid[y - 1][x + 1] == 0) {
+				grid[y][x] = 0;
+			}  
+		}
+	}
+}
 
 int main(int argc, char **argv)
 {
@@ -14,8 +80,11 @@ int main(int argc, char **argv)
 							  SDL_WINDOWPOS_CENTERED,
 							  SDL_WINDOWPOS_CENTERED,
 							  1280, 720,
-							  SDL_WINDOW_ALLOW_HIGHDPI);
+							  SDL_WINDOW_SHOWN);
+	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
+	rule_30();
+	
 	while (running) {
 		SDL_Event e;
 		while (SDL_PollEvent(&e)) {
@@ -25,7 +94,15 @@ int main(int argc, char **argv)
 				break;
 			}
 		}
+		
+		SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+		SDL_RenderClear(renderer);
+
+		render_grid();
+		
+		SDL_RenderPresent(renderer);
 	}
-	
+
+	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 }
