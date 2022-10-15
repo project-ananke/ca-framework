9588825b9811da906f93c8bb802bfb2a13d0fc67
sir-w7 <vohwinston@gmail.com>
2022-10-14 18:41:46 -0700

    Fixed Github actions and build.bat to consider bin directory.

--- build.bat
+++ build.bat
@@ -1,18 +1,19 @@
 @echo off
 
-pushd source/engine/stygatore
+pushd source\engine\stygatore
 go build
 popd
 
 if not exist build mkdir build
+if not exist bin mkdir bin
 
-copy source\engine\stygatore\stygatore.exe build/
-build\stygatore source\engine\generated
+copy source\engine\stygatore\stygatore.exe bin\
+bin\stygatore source\engine\generated
 
 pushd build
-
 cl /nologo /I..\ext\sdl2\include /I..\ext\lua544 ..\source\*.c ..\ext\lua544\*.c ..\source\engine\*.c ..\source\engine\base\*.c ..\source\engine\core\styxlua\*.c ..\source\engine\os\*.c /link /LIBPATH:..\ext\sdl2\lib\x64 SDL2.lib SDL2main.lib Shell32.lib /SUBSYSTEM:CONSOLE /out:ananke.exe
-
-copy ..\ext\sdl2\lib\x64\SDL2.dll .
-
 popd
+
+copy build\ananke.exe bin\
+copy ext\sdl2\lib\x64\SDL2.dll bin\
+
